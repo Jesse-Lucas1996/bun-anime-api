2272e8692e53e6a47d657b4318ab9919d3b97683
Jesse Lucas <jessejlucas@hotmail.com>
2022-07-12 15:19:22 +0800

    init bun with hono anime endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,25 @@ import { Hono } from "hono";
 
 const app = new Hono();
 const port = process.env.PORT || 3000;
+
 const home = app.get("/", (c) => {
-  return c.json({ message: "Hello World!" });
+  return c.json({ message: "Hello World" });
+});
+
+const anime = app.get("/anime", async (c) => {
+  const anime = await fetch("https://animechan.vercel.app/api/random");
+  const res = await anime.json();
+  return c.json({
+    anime: res.anime,
+    character: res.character,
+    quote: res.quote,
+  });
 });
 
 console.log(`Running at http://localhost:${port}`);
 
 export default {
-  port: 3000,
+  port: process.env.PORT || 3000,
   fetch: home.fetch,
+  otherFetch: anime.fetch,
 };
